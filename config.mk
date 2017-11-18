0fedff44e3cf7d1f1edfb15f160320dcc6746bd8
Hiltjo Posthuma <hiltjo@codemadness.org>
2017-11-18 12:28:30 +0100

    Makefile: simplify

--- config.mk
+++ config.mk
@@ -2,29 +2,24 @@
 VERSION = 1.7
 
 # paths
-PREFIX      = /usr/local
-BINDIR      = ${PREFIX}/bin
-MANDIR      = ${PREFIX}/share/man
-MAN1DIR     = ${MANDIR}/man1
-DOCDIR      = ${PREFIX}/share/doc/ii
-
-# Set the following to install to a different root
-DESTDIR     =
-
-INCDIR      = ${PREFIX}/include
-LIBDIR      = ${PREFIX}/lib
+PREFIX   = /usr/local
+BINDIR   = ${PREFIX}/bin
+MANDIR   = ${PREFIX}/share/man
+MAN1DIR  = ${MANDIR}/man1
+DOCDIR   = ${PREFIX}/share/doc/ii
 
 # includes and libs
-INCLUDES    = -I. -I${INCDIR} -I/usr/include
-LIBS        = -L${LIBDIR} -L/usr/lib -lc
+INCLUDES = -I. -I/usr/include
+LIBS     =
 
 # compiler
-CC          = cc
+CC       = cc
 
 # debug
-#CFLAGS      = -g -O0 -pedantic -Wall ${INCLUDES} -DVERSION=\"${VERSION}\" -std=c99 -D_DEFAULT_SOURCE
-#LDFLAGS     = ${LIBS}
+#CFLAGS  = -g -O0 -pedantic -Wall ${INCLUDES} -DVERSION=\"${VERSION}\" \
+#	-std=c99 -D_DEFAULT_SOURCE
+#LDFLAGS = ${LIBS}
 
 # release
-CFLAGS     = -Os ${INCLUDES} -DVERSION=\"${VERSION}\" -std=c99 -D_DEFAULT_SOURCE
-LDFLAGS    = -s
+CFLAGS   = -Os ${INCLUDES} -DVERSION=\"${VERSION}\" -std=c99 -D_DEFAULT_SOURCE
+LDFLAGS  = -s ${LIBS}
